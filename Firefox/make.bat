05c4857a40c172472b365f6a5f721a558cbcd4c1
Benjamin Stürmer <bstuermer@eno.de>
2020-09-04 20:48:28 +0200

    Removed unused files

--- Firefox/make.bat
+++
@@ -1,7 +0,0 @@
-del *.xpi
-set TIMESTAMP=%date%%time: =0%
-set TIMESTAMP=%TIMESTAMP:,=%
-set TIMESTAMP=%TIMESTAMP::=%
-set TIMESTAMP=%TIMESTAMP:.=%
-@echo %TIMESTAMP%
-7za a -tzip passtasticFF%TIMESTAMP%.xpi .\files\*
